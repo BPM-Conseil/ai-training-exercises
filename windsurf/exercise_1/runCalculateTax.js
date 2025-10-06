e9352ed907e296dc88708ed41573b8f82bc6308f
Sebastien Vigroux <sebastien.vigroux@bpm-conseil.com>
2025-10-06 13:26:35 +0200

    Fix run

--- windsurf/exercise_1/runCalculateTax.js
+++ windsurf/exercise_1/runCalculateTax.js
@@ -1,5 +1,10 @@
+/**
+ * Demo script to test the calculateTax function
+ * Runs various test cases and displays results to console
+ */
+
 const calculateTax = require('./calculateTax');
 
 console.log('Price with 20% tax:', calculateTax(100, 0.2));
-console.log('Price with 10% tax:', calculateTax(50, 0.1));
-console.log('Price with 0% tax:', calculateTax(0, 0));
+console.log('Price with 10% tax:', calculateTax(100, 0.1));
+console.log('Price with 0% tax:', calculateTax(100, 0));
